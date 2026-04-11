c340e4277e4165aa3832498c3ac205905261cca3
Tetsuo Nobe <69065590+tetsuo-nobe@users.noreply.github.com>
2026-04-11 21:56:38 +0900

    Update agent_client.py

--- runtime/basicAgent/agent_client.py
+++ runtime/basicAgent/agent_client.py
@@ -8,7 +8,7 @@ payload = json.dumps({"prompt": "こんにちは！"})
 session_id = str(uuid.uuid4())
 
 response = client.invoke_agent_runtime(
-    agentRuntimeArn='arn:aws:bedrock-agentcore:us-west-2:068048081706:runtime/myagent-RcLFq55IZq',
+    agentRuntimeArn='arn:aws:bedrock-agentcore:us-west-2:123456789012:runtime/myagent-RcLFq55IZq',
     runtimeSessionId=session_id, # Must be 33+ char. Every new SessionId will create a new MicroVM
     payload=payload
 )
